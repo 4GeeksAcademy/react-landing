6295ee336c8d87ad42c84297070559f374cde0db
stewybenz95 <113649226+stewybenz95@users.noreply.github.com>
2023-05-21 01:01:46 +0000

    first commit

---
+++ src/js/component/navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+const Navbar = () => {
+	return (
+        <div></div>
+    )}
+
+export default Navbar;
